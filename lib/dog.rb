93f6d73c8248f810921b214f4876f3f3e36f97e7
Christopher Osborn <z68ozz@gmail.com>
2020-05-10 01:17:27 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+class Dog 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end
+  
+  def self.all 
+    @@all 
+  end
+  
+  def self.clear_all 
+    @@all.clear 
+  end 
+  
+  def self.print_all 
+    @@all.each {|d| puts "#{d}"}
+  end 
+end 
+  
